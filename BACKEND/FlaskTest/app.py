bb3a30eb29e4ba5eda2ec9d18c19b73e80a3307a
greennarae <smk5647@gmail.com>
2022-10-06 12:49:36 +0900

    Set Cookie 예제 추가

--- BACKEND/FlaskTest/app.py
+++ BACKEND/FlaskTest/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, make_response, request
 
 #Flask 객체 인스턴스 생성
 app = Flask(__name__)
@@ -25,3 +25,17 @@ def index():
 @app.route('/abcd')
 def abcdefg(): # 적절하지 못한 함수이름. 즉, 임의로 설정가능.
   return "<h1>Hello Python</h1>"
+
+# RESTful 하지않은 API설계의 대표적 예시. 동사를 사용하지 말고 명사를 써야함. 즉, /cookie 등이 올바름.
+# 왜? -> 사용자는 get method 로 http 요청을 보낼것이고 이 자체가 give me(동사부분) 라는 의미를 담고 있음.
+@app.route('/giveMeCookie') 
+def cookieHandle():
+  res = make_response("Create Cookie!!")
+  res.set_cookie("myCookieName", "FunnyCookie")
+  return res
+# 위 URL로 접속하고, 개발자도구(F12)를 열어 Application, Cookies 를 확인하면 쿠키가 등록된것을 볼수있음.
+
+
+if __name__=="__main__":
+  app.run(host="127.0.0.1", port="8000", debug=True)
+  # 반드시 poetry run python app.py(파일이름) 커맨드로 실행할것.
