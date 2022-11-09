5d97e5849f43e81cd8a2b5bc000303dad560df7d
sooyeon <wndqhr__@naver.com>
2022-11-10 03:07:19 +0900

    add QR page

--- fe/src/index.jsx
+++ fe/src/index.jsx
@@ -2,19 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {CookiesProvider} from 'react-cookie';
-// import App from './App';
-// import ShopName from './Start/ShopName';
-// import OrderButton from './Start/OrderButton';
-// import Start from './Start/Start';
-// import QR from './QR/QR';
-import QrBox from './QR/components/QrBox';
-// import Cookie from './QR/components/Cookie';
+import App from './App';
+// import QrBox from './QR/QrBox';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <CookiesProvider>
-      <QrBox />
+      <App />
     </CookiesProvider>
   </React.StrictMode>
 );
