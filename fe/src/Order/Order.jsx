2aad83a83d7c00c1f0bbb45aedd2b49225b88c8d
ryeeun <dmsdl7531@naver.com>
2022-11-09 21:37:59 +0900

    feat : 카테고리 페이지 수정

--- fe/src/Order/Order.jsx
+++ fe/src/Order/Order.jsx
@@ -3,60 +3,14 @@ import React, { useState } from 'react';
 import Header from '../components/Header';
 import './Order.css';
 
-const data = [
-  {
-    category_name: '에스프레소',
-    menus: [
-      {
-        menu_id: 1,
-        name: '아메리카노',
-        tag: '#샷 #물',
-        price: '4,500원',
-        img: 'americano',
-      },
-      {
-        menu_id: 2,
-        name: '카페 라떼',
-        tag: '#샷 #우유',
-        price: '5,000원',
-        img: 'caffelatte',
-      },
-      {
-        menu_id: 3,
-        name: '돌체라떼',
-        tag: '#샷 #무지방 우유 #돌체 시럽',
-        price: '5,500원',
-        img: 'dolcelatte',
-      },
-    ],
-  },
-  {
-    category_name: '블렌디드',
-    menus: [
-      {
-        menu_id: 4,
-        name: '딸기 딜라이트 요거트 블렌디드',
-        tag: '#딸기 #우유',
-        price: '6,300원',
-        img: 'strawberry',
-      },
-      {
-        menu_id: 5,
-        name: '망고 바나나 블렌디드',
-        tag: '#망고 #바나나 #우유',
-        price: '6,300원',
-        img: 'mangobanana',
-      },
-    ],
-  },
-];
+const data = ['콜드 브루 커피', '에스프레소', '프라푸치노', '블렌디드'];
 
 const Order = function () {
   const [catArr, setArr] = useState(
     data.map((elem, index) => {
       const category = {
         id: index,
-        name: elem.category_name,
+        name: elem,
         active: index === 0,
       };
       return category;
@@ -79,7 +33,7 @@ const Order = function () {
 
   return (
     <div className="order">
-      <Header />
+      <Header text="주문" />
       <div className="order-category">
         {catArr.map((elem) => (
           <button
