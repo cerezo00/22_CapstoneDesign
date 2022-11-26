2e5e96c6f8b890d04cb20e8703b9ab0a56aa4801
ryeeun <dmsdl7531@naver.com>
2022-11-26 13:06:06 +0900

    feat : qr 단일 상품 데이터 타입 변경

--- fe/src/QR/QrBox.jsx
+++ fe/src/QR/QrBox.jsx
@@ -1,11 +1,11 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import QRCode from "qrcode.react";
-import Header from "../components/Header";
-import QRButton from "./QRButton";
-import "./QRButton.css";
-import "../components/css/Header.css";
-import "./QrBox.css";
+import QRCode from 'qrcode.react';
+import Header from '../components/Header';
+import QRButton from './QRButton';
+import './QRButton.css';
+import '../components/css/Header.css';
+import './QrBox.css';
 
 /*
 const datas = [
@@ -35,44 +35,48 @@ const datas = [
 */
 
 function QrBox() {
-    const [items, setItems] = useState("");
-    const { state } = useLocation();
-    // eslint-disable-next-line
-    console.log(state);
+  const [items, setItems] = useState('');
+  const { state } = useLocation();
+  // eslint-disable-next-line
+  console.log(state);
 
-    useEffect(() => {
-        if(state == null) {
-            setItems(localStorage.getItem('shoppingCart'));
-            // setItems(JSON.stringify(datas));
-        } else {
-            setItems(state);
-        }
-    });
+  useEffect(() => {
+    if (state == null) {
+      setItems(localStorage.getItem('shoppingCart'));
+      // setItems(JSON.stringify(datas));
+    } else {
+      setItems(JSON.stringify([state.item]));
+    }
+  }, []);
 
-    // eslint-disable-next-line
-    console.log(JSON.stringify(items));
-    // eslint-disable-next-line
-    // console.log(JSON.parse(items));
+  // eslint-disable-next-line
+  console.log(JSON.stringify(items));
+  // eslint-disable-next-line
+  // console.log(JSON.parse(items));
 
-    return( 
-        <div className="QrBox-container">
-            <Header text="결제하기" />
-            <div className="QrBox-message">
-                <div className="QrBox-scan">
-                    QR을 리더기에 스캔해주세요.
-                </div>
-                <div className="QrBox-guide">
-                    <span className="QrBox-span">
-                        키오스크 또는 직원
-                    </span>을 통해 QR로 결제하세요!<br/>
-                </div>
-                <div className="QrBox-QR-container">
-                <QRCode className="QrBox-QRcode" id = "qr-gen" size={250} value={items} includeMargin="true"/>
-                </div>
-                </div>
-            <QRButton />
+  return (
+    <div className="QrBox-container">
+      <Header text="결제하기" />
+      <div className="QrBox-message">
+        <div className="QrBox-scan">QR을 리더기에 스캔해주세요.</div>
+        <div className="QrBox-guide">
+          <span className="QrBox-span">키오스크 또는 직원</span>을 통해 QR로
+          결제하세요!
+          <br />
         </div>
-    );
+        <div className="QrBox-QR-container">
+          <QRCode
+            className="QrBox-QRcode"
+            id="qr-gen"
+            size={250}
+            value={items}
+            includeMargin="true"
+          />
+        </div>
+      </div>
+      <QRButton />
+    </div>
+  );
 }
 
 export default QrBox;
